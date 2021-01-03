c4366734c172ae1e8306f04dbedd0c6af28a4dcb
markgmz92 <markgmz92@gmail.com>
2021-01-03 18:25:37 +0800

    [BB - 003] created header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import Header from './components/ui/Header';
+
 import './App.css';
 
 const App = () => {
-  return <div className="container">Hello</div>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
