24e28a2b98ee2f9236875569a90a2ae4fc533f71
markgmz92 <markgmz92@gmail.com>
2021-01-04 16:35:30 +0800

    [BB - 006] Passing search query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/ui/Header';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 
 import './App.css';
 import axios from 'axios';
@@ -8,11 +9,12 @@ import axios from 'axios';
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
 
       console.log(result.data);
@@ -21,11 +23,12 @@ const App = () => {
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
