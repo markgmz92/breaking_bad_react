4aa76bb33f211a29c78369e3659362975a9a3413
markgmz92 <markgmz92@gmail.com>
2021-01-04 15:23:18 +0800

    [BB - 005] Completed CharacterItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,23 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import logo from '../../img/logo.png';
 
 const Header = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
-      );
-
-      console.log(result.data);
-    };
-
-    fetchItems();
-  }, []);
-
   return (
     <header className="center">
       <img src={logo} alt="" />
